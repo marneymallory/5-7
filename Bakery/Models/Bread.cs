b8663e2acacecedfae2f0202c820bbf67c29d27d
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-07 15:02:53 -0800

    add passing test for BreadConstructor

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,12 +5,22 @@ namespace Bakery.Models
 { 
   public class Bread 
   {
-
+    public int Amount { get; set; }
+    private static List<Bread> _instances = new List<Bread> { };
+    public static List<Bread> GetAll()
+    {
+      return _instances;
+    }
+     public Bread(int amount)
+    {
+      Amount = amount;
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
-  
-
-
-
 
 }
 
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -5,6 +5,17 @@ namespace Bakery.Models
 { 
   public class Pastry
   {
-
+    public int Amount { get; set; }
+    public int Price { get; set; }
+    private static List<Pastry> _instances = new List<Pastry> { };
+    public static List<Pastry> GetAll()
+    {
+      return _instances;
+    } 
+     public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
+
 }
