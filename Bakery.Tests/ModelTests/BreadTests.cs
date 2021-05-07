74a11366af249db7045364f3fc77ae4c9d3a4147
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-07 15:30:23 -0800

    add good fail for GetPrice test

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -18,7 +18,14 @@ namespace Bakery.TestTools
       Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
-    
+    [TestMethod]
+    public void GetPrice_ReturnPrice_Int()
+    {
+     int amount = 1;
+     Bread newBread = new Bread(amount);
+    int result = newBread.GetPrice();
+    Assert.AreEqual(5, result);
+    }
 
   }
 }
